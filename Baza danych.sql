a92dffb575298d26d0877c4a6d51263c898c3c10
3Rii <101877840+3Rii@users.noreply.github.com>
2022-04-21 21:22:58 +0200

    Update and rename Baza danych.txt to Baza danych.sql

--- Baza danych.sql
+++ Baza danych.sql
@@ -1,15 +1,14 @@
 # Tabela z kontami
-
 Create date database `projekt`;
 
 Use `projekt`;
 
 CREATE TABLE IF NOT EXISTS `accounts` (
-	`id` int(11) NOT NULL AUTO_INCREMENT,
+    `id` int(11) NOT NULL AUTO_INCREMENT,
     `fullname` varchar(50) NOT NULL,
-  	`username` varchar(50) NOT NULL,
-  	`password` varchar(255) NOT NULL,
-  	`email` varchar(100) NOT NULL,
+      `username` varchar(50), UNIQUE NOT NULL,
+      `password` varchar(255) NOT NULL,
+      `email` varchar(100) UNIQUE, NOT NULL,
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
