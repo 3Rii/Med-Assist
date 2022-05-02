24e4640eb1d0223e9c9d1dbf69703c87b9dc940a
KGranosik <103218501+KGranosik@users.noreply.github.com>
2022-05-02 18:06:43 +0200

    Update projekt.sql

--- projekt.sql
+++ projekt.sql
@@ -1,80 +1,132 @@
-CREATE TABLE accounts (
-    id       NUMBER NOT NULL,
-    fullname VARCHAR2(25) NOT NULL,
-    username VARCHAR2(25) NOT NULL,
-    password VARCHAR2(25) NOT NULL,
-    email    VARCHAR2(25)
-);
-
-ALTER TABLE accounts ADD CONSTRAINT accounts_pk PRIMARY KEY ( id );
-
-ALTER TABLE accounts ADD CONSTRAINT accounts_username_un UNIQUE ( username );
-
-CREATE TABLE basics (
-    id          NUMBER NOT NULL,
-    wiek        NUMBER(2) NOT NULL,
-    waga        NUMBER(3) NOT NULL,
-    wzrost      NUMBER(3) NOT NULL,
-    plec        CHAR(1) NOT NULL,
-    papierosy   CHAR(1) NOT NULL,
-    alkohol     CHAR(1) NOT NULL,
-    aktywnosc   VARCHAR2(25) NOT NULL,
-    accounts_id NUMBER NOT NULL
-);
-
-CREATE UNIQUE INDEX basics__idx ON
-    basics (
-        accounts_id
-    ASC );
-
-ALTER TABLE basics ADD CONSTRAINT basics_pk PRIMARY KEY ( id );
-
-CREATE TABLE checkups (
-    id        NUMBER NOT NULL,
-    nazwa     VARCHAR2(25) NOT NULL,
-    status    VARCHAR2(25) NOT NULL,
-    basics_id NUMBER NOT NULL
-);
-
-ALTER TABLE checkups ADD CONSTRAINT checkups_pk PRIMARY KEY ( id );
-
-CREATE TABLE entity_6 
-    
-    -- No Columns 
+CREATE TABLE accounts 
+    (
+     id NUMERIC (28) NOT NULL , 
+     fullname VARCHAR (25) NOT NULL , 
+     username VARCHAR (25) NOT NULL , 
+     password VARCHAR (25) NOT NULL , 
+     email VARCHAR (25) 
+    )
 ;
 
-CREATE TABLE prevention (
-    id        NUMBER NOT NULL,
-    nazwa     VARCHAR2(25) NOT NULL,
-    link      VARCHAR2(25) NOT NULL,
-    status    VARCHAR2(25) NOT NULL,
-    basics_id NUMBER NOT NULL
-);
+ALTER TABLE accounts ADD CONSTRAINT accounts_pk PRIMARY KEY CLUSTERED (id)
+    ;
+    ALTER TABLE accounts ADD CONSTRAINT accounts_username_un UNIQUE NONCLUSTERED (username)
+    ;
 
-ALTER TABLE prevention ADD CONSTRAINT prevention_pk PRIMARY KEY ( id );
+CREATE TABLE basics 
+    (
+     id NUMERIC (28) NOT NULL , 
+     wiek NUMERIC (2) NOT NULL , 
+     waga NUMERIC (3) NOT NULL , 
+     wzrost NUMERIC (3) NOT NULL , 
+     plec CHAR (1) NOT NULL , 
+     papierosy CHAR (1) NOT NULL , 
+     alkohol CHAR (1) NOT NULL , 
+     aktywnosc VARCHAR (25) NOT NULL , 
+     accounts_id NUMERIC (28) NOT NULL 
+    )
+; 
 
-CREATE TABLE vaccines (
-    id        NUMBER NOT NULL,
-    nazwa     VARCHAR2(25) NOT NULL,
-    typ       VARCHAR2(25) NOT NULL,
-    status    VARCHAR2(25) NOT NULL,
-    basics_id NUMBER NOT NULL
-);
 
-ALTER TABLE vaccines ADD CONSTRAINT vaccines_pk PRIMARY KEY ( id );
+    
 
-ALTER TABLE basics
-    ADD CONSTRAINT basics_accounts_fk FOREIGN KEY ( accounts_id )
-        REFERENCES accounts ( id );
 
-ALTER TABLE checkups
-    ADD CONSTRAINT checkups_basics_fk FOREIGN KEY ( basics_id )
-        REFERENCES basics ( id );
+CREATE UNIQUE INDEX 
+    basics__idx ON basics 
+    ( 
+     accounts_id 
+    ) 
+;
 
-ALTER TABLE prevention
-    ADD CONSTRAINT prevention_basics_fk FOREIGN KEY ( basics_id )
-        REFERENCES basics ( id );
+ALTER TABLE basics ADD CONSTRAINT basics_pk PRIMARY KEY CLUSTERED (id)
+    ;
 
-ALTER TABLE vaccines
-    ADD CONSTRAINT vaccines_basics_fk FOREIGN KEY ( basics_id )
-        REFERENCES basics ( id );
+CREATE TABLE checkups 
+    (
+     id NUMERIC (28) NOT NULL , 
+     nazwa VARCHAR (25) NOT NULL , 
+     status VARCHAR (25) NOT NULL , 
+     basics_id NUMERIC (28) NOT NULL 
+    )
+;
+
+ALTER TABLE checkups ADD CONSTRAINT checkups_pk PRIMARY KEY CLUSTERED (id)
+    ;
+
+CREATE TABLE prevention 
+    (
+     id NUMERIC (28) NOT NULL , 
+     nazwa VARCHAR (25) NOT NULL , 
+     link VARCHAR (25) NOT NULL , 
+     status VARCHAR (25) NOT NULL , 
+     basics_id NUMERIC (28) NOT NULL 
+    )
+;
+
+ALTER TABLE prevention ADD CONSTRAINT prevention_pk PRIMARY KEY CLUSTERED (id)
+    ;
+
+CREATE TABLE vaccines 
+    (
+     id NUMERIC (28) NOT NULL , 
+     nazwa VARCHAR (25) NOT NULL , 
+     typ VARCHAR (25) NOT NULL , 
+     status VARCHAR (25) NOT NULL , 
+     basics_id NUMERIC (28) NOT NULL 
+    )
+;
+
+ALTER TABLE vaccines ADD CONSTRAINT vaccines_pk PRIMARY KEY CLUSTERED (id)
+    ;
+
+ALTER TABLE basics 
+    ADD CONSTRAINT basics_accounts_fk FOREIGN KEY 
+    ( 
+     accounts_id
+    ) 
+    REFERENCES accounts 
+    ( 
+     id 
+    ) 
+    ON DELETE NO ACTION 
+    ON UPDATE NO ACTION 
+;
+
+ALTER TABLE checkups 
+    ADD CONSTRAINT checkups_basics_fk FOREIGN KEY 
+    ( 
+     basics_id
+    ) 
+    REFERENCES basics 
+    ( 
+     id 
+    ) 
+    ON DELETE NO ACTION 
+    ON UPDATE NO ACTION 
+;
+
+ALTER TABLE prevention 
+    ADD CONSTRAINT prevention_basics_fk FOREIGN KEY 
+    ( 
+     basics_id
+    ) 
+    REFERENCES basics 
+    ( 
+     id 
+    ) 
+    ON DELETE NO ACTION 
+    ON UPDATE NO ACTION 
+;
+
+ALTER TABLE vaccines 
+    ADD CONSTRAINT vaccines_basics_fk FOREIGN KEY 
+    ( 
+     basics_id
+    ) 
+    REFERENCES basics 
+    ( 
+     id 
+    ) 
+    ON DELETE NO ACTION 
+    ON UPDATE NO ACTION 
+;
