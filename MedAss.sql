b482c6fcd1553ac07a25e5cdbcb5e3cfbbe83a39
KGranosik <103218501+KGranosik@users.noreply.github.com>
2022-05-12 08:58:36 +0200

    link_gov       char(20) ->char(100)

--- MedAss.sql
+++ MedAss.sql
@@ -2,75 +2,70 @@ use projekt;
 
 create table accounts
 (
-    id       int auto_increment,
+    id       int auto_increment
+        primary key,
     fullname char(50) not null,
     username char(20) not null,
     password char(20) not null,
-    email    char(20) not null,
-    constraint accounts_pk
-        primary key (id)
-);
-
-create table user
-(
-    id int auto_increment,
-    wiek float null,
-    waga float null,
-    wzrost float null,
-    plec bool null,
-    papierosy bool null,
-    alkohol bool null,
-    aktywnosc bool null,
-    constraint vaccines_pk
-      primary key(id),
-    constraint vaccines_accounts_id_fk
-      foreign key (id) references accounts (id)
-);
+    email    char(40) not null
+)
+    auto_increment = 8;
 
 create table check_ups
 (
-    id    int auto_increment,
-    nazwa char(20) not null,
-    constraint check_ups_pk
-        primary key (id),
+    id             int auto_increment
+        primary key,
+    id_user        int                    null,
+    current_status tinyint(1) default 0   null,
+    todo_status    tinyint(1) default 0   null,
+    nazwa          char(20)   default '0' null,
     constraint check_ups_accounts_id_fk
-        foreign key (id) references accounts (id)
+        foreign key (id_user) references accounts (id)
 );
 
 create table prevention
 (
-    id       int auto_increment,
-    nazwa    char(20) not null,
-    link_gov char(20) not null,
-    constraint prevention_pk
-        primary key (id),
+    id             int auto_increment
+        primary key,
+    id_user        int                    null,
+    link_gov       char(100)   default '0' not null,
+    current_status tinyint(1) default 0   null,
+    todo_status    tinyint(1) default 0   null,
+    nazwa          char(20)   default '0' not null,
     constraint prevention_accounts_id_fk
-        foreign key (id) references accounts (id)
+        foreign key (id_user) references accounts (id)
 );
+
+create table user
+(
+    id         int auto_increment
+        primary key,
+    account_id int                  not null,
+    wiek       float      default 0 null,
+    waga       float      default 0 null,
+    wzrost     float      default 0 null,
+    plec       tinyint(1) default 0 null,
+    papierosy  tinyint(1) default 0 null,
+    alkohol    tinyint(1) default 0 null,
+    aktywnosc  tinyint(1) default 0 null,
+    constraint user_u_id_uindex
+        unique (id),
+    constraint user_ibfk_1
+        foreign key (account_id) references accounts (id),
+    constraint user_accounts_id_fk
+        foreign key (account_id) references accounts (id)
+)
+    auto_increment = 10;
 
 create table vacc
 (
-    id       int auto_increment,
-    nazwa    char(20) not null,
-    typ char(20) not null,
-    constraint vacc_pk
-        primary key (id),
+    id             int auto_increment
+        primary key,
+    id_user        int                    null,
+    typ            char(20)   default '0' not null,
+    current_status tinyint(1) default 0   null,
+    todo_status    tinyint(1) default 0   null,
+    nazwa          char(20)   default '0' not null,
     constraint vacc_accounts_id_fk
-        foreign key (id) references accounts (id)
+        foreign key (id_user) references accounts (id)
 );
-alter table check_ups
-    add current_status bool null;
-
-alter table check_ups
-    add todo_status bool null;
-
-alter table prevention
-    add current_status bool null;
-
-alter table prevention
-    add todo_status bool null;
-alter table vacc
-    add current_status bool null;
-
-alter table vacc
-    add todo_status bool null;
